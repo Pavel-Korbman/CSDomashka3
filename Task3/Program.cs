f996c863ad2edb934391ac2bcf7d41a9bd5c20c4
Pavel <pavel@b2profit.ru>
2023-10-20 17:48:55 +0300

    Решил 3 задачу

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+for(int i =1; i<=N; i++)
+{
+    Console.WriteLine(Math.Pow(i,3));
+}
