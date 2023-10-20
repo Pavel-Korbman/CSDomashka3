b4b327aeddbe043e5f97f998e0bd2d0926acd2b2
Pavel <pavel@b2profit.ru>
2023-10-20 16:20:10 +0300

    Решил 1 задачу

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите пятизначное число");
+int number = Convert.ToInt32 (Console.ReadLine());
+bool IsPalindrome(int number)
+{ 
+    if (number < 10000 || number > 99999)
+       {
+       Console.WriteLine("Число не пятизначное");
+       return false;
+       }
+    else
+    {
+      if (number / 10000 == number % 10 && number % 10000 / 1000 == number % 100 / 10)
+        {
+         return true;
+        }
+     return false;
+    }
+}   
+
+   Console.WriteLine(IsPalindrome(number));  
