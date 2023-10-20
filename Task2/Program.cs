56ef0d007bf372edda4a96a8d8dfca4ec87d0ec5
Pavel <pavel@b2profit.ru>
2023-10-20 17:27:30 +0300

    Решил 2 задачу

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] pointA = {3, 6, 8};
+int[] pointB = {2, 1, -7};
+double Length(int[] pointA, int[] pointB)
+{
+double dist = Math.Round (Math.Sqrt(Math.Pow((pointB[0]-pointA[0]),2) + Math.Pow((pointB[1]-pointA[1]),2) + Math.Pow((pointB[2]-pointA[2]),2)), 2);
+return dist;
+}
+Console.WriteLine (Length(pointA, pointB));
